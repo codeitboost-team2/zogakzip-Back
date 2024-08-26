89291c6ead57bd877b8ece1d4196f8cfa0711988
uzzinni <yujin7154@gmail.com>
2024-08-27 00:35:07 +0900

    [FEAT] 이미지 API 구현

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import groupRouter from './routes/groupRoutes.js';
+import imageRouter from './routes/imageRoutes.js';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/api/groups', groupRouter);
+app.use('/api/images', imageRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
