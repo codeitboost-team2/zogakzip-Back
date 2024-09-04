b50aa70d2c5aa0b3d16c3b2f371df4cdd33ae06b
uzzinni <yujin7154@gmail.com>
2024-09-04 09:13:51 +0900

    [MOD] 이미지 API 수정

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,26 +1,38 @@
-import { PrismaClient } from '@prisma/client';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { createImage } from '../models/imageModel.js';
 
-const prisma = new PrismaClient();
+// __dirname 대체
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const uploadImage = async (req, res) => {
-    if (req.file) {
-        const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
-        
-        // 데이터베이스에 이미지 정보 저장
-        try {
-            const savedImage = await prisma.image.create({
-                data: {
-                    filename: req.file.filename,
-                    url: imageUrl,
-                },
-            });
-
-            res.status(200).json({ imageUrl: savedImage.url });
-        } catch (error) {
-            console.error('Error saving image to DB:', error);
-            res.status(500).send('Error saving image to database.');
-        }
-    } else {
-        res.status(400).send('No file uploaded.');
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
     }
+
+    const file = req.file;
+    const filename = Date.now() + '-' + file.originalname;
+    const uploadDir = path.join(__dirname, '../uploads');
+    const uploadPath = path.join(uploadDir, filename);
+
+    // uploads 폴더가 없으면 생성
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+
+    // 파일을 서버의 지정된 경로에 저장
+    fs.writeFileSync(uploadPath, file.buffer);
+
+    // 이미지 정보를 데이터베이스에 저장
+    const image = await createImage(filename, `/uploads/${filename}`);
+
+    // 클라이언트에 이미지 URL 응답
+    res.status(200).json({ imageUrl: image.url });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to upload image' });
+  }
 };
--- models/imageModel.js
+++ models/imageModel.js
@@ -1,16 +1,12 @@
-import multer from 'multer';
-import path from 'path';
+import { PrismaClient } from '@prisma/client';
 
-// 파일 저장을 위한 multer 설정
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');  // 파일이 저장될 경로
+const prisma = new PrismaClient();
+
+export const createImage = async (filename, url) => {
+  return await prisma.image.create({
+    data: {
+      filename,
+      url,
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);  // 파일 이름 설정
-    }
-});
-
-const upload = multer({ storage: storage });
-
-export default upload;
+  });
+};
